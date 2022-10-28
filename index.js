7e60d9b1ecb0b89d5225ee13099df2f006a10f8e
mino42 <peter304023@gmail.com>
2022-10-28 16:49:28 +0200

    alkamlazas sema feltoltese

--- index.js
+++ index.js
@@ -14,28 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(routeDog);
 app.use(routeUser);
 
-<<<<<<< HEAD
 app.listen(PORT, () => {
   console.log("App listening at: http://localhost:8080/");
 });
-=======
-const express = require('express')
-
-
-const server = express()
-server.use(express.json())
-
-server.get('/', (req, res) => {
-  res.json({ message: 'Szia Geri vagyok!' })
-})
-
-
-
-module.exports = server
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log(`\n*** Server running on port ${port} ***\n`);
-});
->>>>>>> c977955024f15790d693724bd57b0ac12a4ab47e
