8b315fe820c1e5832b1c450c4c917405c9afa031
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-07 01:26:53 +0100

    adatfeltoltesproba

--- dao/film-dao.js
+++ dao/film-dao.js
@@ -54,7 +54,6 @@ class FilmDAO {
 	}
 
 	async filmfeltoltes(cim, leiras, megjelenes, elozetesLink) {
-		let id=2;
 		let results = await db.query("INSERT INTO film (cim, leiras, megjelenes, \"elozetesLink\") VALUES ($1, $2, $3, $4)", [cim, leiras, megjelenes, elozetesLink]).catch(console.log);
 			return results;
 	}
