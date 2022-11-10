c67cd5e5dba19df4d2e35feb23b4199800ba73cb
Németh Zoltán <h162405@stud.u-szeged.hu>
2022-11-10 21:27:32 +0100

    Upload New File

---
+++ public/classes/jelszo.js
@@ -0,0 +1,26 @@
+npm i bcrypt
+
+const bcrypt = require('bcrypt');
+async function hashIt(password){
+    const salt = await bcrypt.genSalt(6);
+    const hashed = await bcrypt.hash(password, salt);
+}
+hashIt(password);
+// compare the password user entered with hashed pass.
+async function compareIt(password){
+    const validPassword = await bcrypt.compare(password, hashedPassword);
+}
+compareIt(password);
+
+const bcrypt = require('bcryptjs'); // npm i bcryptjs
+const password = "12345"
+let salt = bcrypt.genSaltSync(10);
+let hash = bcrypt.hashSync(password, salt);
+// check hash password
+// bcrypt.compareSync(password, hashPassword);
+
+//hash password
+const hashedPassword = bcrypt.hashSync(yourPasswordFromSignupForm, bcrypt.genSaltSync());
+
+//verify password
+const doesPasswordMatch = bcrypt.compareSync(yourPasswordFromLoginForm, yourHashedPassword)
