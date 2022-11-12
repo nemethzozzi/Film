5fedb7d9a8972e0e0d8a147661ee820c9a0c8f64
Nagy Maximilián <h162128@stud.u-szeged.hu>
2022-11-12 18:48:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const app = express();
 const routeUser = require('./routes/route-users');
 const PORT  = process.env.PORT || 8080;
 const cookieParser = require("cookie-parser");
+const routeMyUser = require('./routes/route-myUsers');
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(routeUser);
+app.use(routeMyUser);
 
 //form-ok és method = POST miatt
 app.use(express.json());
