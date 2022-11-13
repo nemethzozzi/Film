ea84a27ab9ec5e4197c3da4009c546681342c7b7
Nagy Maximilián <h162128@stud.u-szeged.hu>
2022-11-13 16:48:10 +0100

    Update route-myUsers.js

--- routes/route-myUsers.js
+++ routes/route-myUsers.js
@@ -13,14 +13,32 @@ router.post("/userLogin", async (req,res) => {
     var password = req.body.password;
 
     const user = await new myUserDAO().getUserDataByEmail(email);
-    bcrypt.compare(password, user[4]).then(function(result){
+    bcrypt.compare(password, user.jelszo).then(function(result){
         if(result){
-            sessionStorage.setItem("fId", user[0]);
-            sessionStorage.setItem("fNev", user[1]);
-            sessionStorage.setItem("fTipus", user[2]);
-            sessionStorage.setItem("fEmail", user[3]);
-            alert("you are logged in as: $1, $2, $3, $4", user[0], user[1], user[2], user[3]);
+            sessionStorage.setItem("fId", user.id);
+            sessionStorage.setItem("fNev", user.nev);
+            sessionStorage.setItem("fTipus", user.tipus);
+            sessionStorage.setItem("fEmail", user.email);
+            alert("you are logged in as: $1, $2, $3, $4", user.nev, user.email, user.tipus, user.id);
         }else{alert("wrong credentials! no login for you! >:(")}
     });
         
 }); 
+router.post("/userRegister", async (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    var nev = req.body.password;
+
+    myUserDAO().ujFelhasznalo(nev, email, jelszo);
+    const user = await new myUserDAO().getUserDataByEmail(email);
+    bcrypt.compare(password, user.jelszo).then(function(result){
+        if(result){
+            sessionStorage.setItem("fId", user.id);
+            sessionStorage.setItem("fNev", user.nev);
+            sessionStorage.setItem("fTipus", user.tipus);
+            sessionStorage.setItem("fEmail", user.email);
+            alert("you are logged in as: $1, $2, $3, $4", user.nev, user.email, user.tipus, user.id);
+        }else{alert("wrong credentials! no login for you! >:(")}
+    });
+
+});
