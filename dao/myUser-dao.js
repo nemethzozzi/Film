f2e5f6ac9623a0eb1cb4664ee38af60b9b9a07f5
Bánki Gergely <bankigergely0519@gmail.com>
2022-11-14 19:20:41 +0100

    update

--- dao/myUser-dao.js
+++ dao/myUser-dao.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 class myUserDAO {
     async getUserByEmail(email){
         // visszaad egy változót ami tárolja a lekérdezés eredményeit, email alapján
-        let dbQuery = await db.query('SELECT id, nev, tipus, email, jelszo FROM Felhasznalo WHERE email = $1', [email]);
+        let dbQuery = await db.query("SELECT id, nev, tipus, email, jelszo FROM Felhasznalo WHERE email = $1", [email]);
         if(dbQuery = null){
             return 'no match';
         }
@@ -18,11 +18,11 @@ class myUserDAO {
         //update: tejlesen obsolete, később törlés
 
         let userData = new userData[5];
-        userData[0] =  await db.query('SELECT id FROM Felhasznalo WHERE email = $1', [email]);
-        userData[1] =  await db.query('SELECT nev FROM Felhasznalo WHERE email = $1', [email]);
-        userData[2] =  await db.query('SELECT tipus FROM Felhasznalo WHERE email = $1', [email]);
+        userData[0] =  await db.query("SELECT id FROM Felhasznalo WHERE email = $1", [email]);
+        userData[1] =  await db.query("SELECT nev FROM Felhasznalo WHERE email = $1", [email]);
+        userData[2] =  await db.query("SELECT tipus FROM Felhasznalo WHERE email = $1", [email]);
         userData[3] =  email;
-        userData[4] =  await db.query('SELECT jelszo FROM Felhasznalo WHERE email = $1', [email]);
+        userData[4] =  await db.query("SELECT jelszo FROM Felhasznalo WHERE email = $1", [email]);
         return userData;
     }
 
