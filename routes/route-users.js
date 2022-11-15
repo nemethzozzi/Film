a8ff07dea22a525e9bb76ab1bac680702a76eabe
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-15 19:20:44 +0100

    szures

--- routes/route-users.js
+++ routes/route-users.js
@@ -154,13 +154,6 @@ router.get("/megnezendo", async(req, res) => {
 router.post("/szurproba", async(req, res) => {
 	let kaptam= JSON.parse(req.body.feltetelek);
 	let lekerdezes = await new FilmDAO().szures(kaptam['rendezok'],kaptam['szineszek'],kaptam['mufaj'],kaptam['ev']);
-	let asd={};
-	/*asd['asd']='dsa';
-	for (var i=0; i<lekerdezes.length; i++){
-		
-		
-	}*/
-	
 	res.setHeader('Content-Type', 'application/json');
     res.end(/*string ide*/''+JSON.stringify(lekerdezes)+'');
 });
