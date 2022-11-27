2dce91d6e33775f133ebd378d1449d1910199e48
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-27 17:02:20 +0100

    komment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'eisebavoesxwhl',
-    host: 'ec2-54-76-43-89.eu-west-1.compute.amazonaws.com',
-    database: 'd4c6fa70e6ag0v',
-    password: '9bd35dd001a022996f482f944df1283dcead71b52485a2be7b0c63508cbc3d6f',
-    port: 5432,
+    user: 'doadmin',
+    host: 'db-postgresql-fra1-96846-do-user-12971066-0.b.db.ondigitalocean.com',
+    database: 'film',
+    password: 'AVNS_0rXk4wkKmqQa6uONWpo',
+    port: 25060,
     ssl: {
         rejectUnauthorized: false
     }
