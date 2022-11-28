e5a1c83ebe33065a4ae73ee963c932ddb8d3fd30
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-28 17:07:35 +0100

    fooldal menuk

--- config/db.js
+++ config/db.js
@@ -12,11 +12,11 @@ const pool = new Pool({
     }*/
 	
 	//régiek:
-	user: 'doadmin',
-    host: 'db-postgresql-fra1-96846-do-user-12971066-0.b.db.ondigitalocean.com',
-    database: 'film',
-    password: 'AVNS_0rXk4wkKmqQa6uONWpo',
-    port: 25060,
+	user: 'eisebavoesxwhl',
+    host: 'ec2-54-76-43-89.eu-west-1.compute.amazonaws.com',
+    database: 'd4c6fa70e6ag0v',
+    password: '9bd35dd001a022996f482f944df1283dcead71b52485a2be7b0c63508cbc3d6f',
+    port: 5432,
 	ssl: {
         rejectUnauthorized: false
     }
