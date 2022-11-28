05d262924695eaff7d32d898720d420bedd9616a
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-28 17:05:54 +0100

    fooldal menuk

--- config/db.js
+++ config/db.js
@@ -1,14 +1,26 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'doadmin',
+    //újak:
+	/*user: 'doadmin',
     host: 'db-postgresql-fra1-96846-do-user-12971066-0.b.db.ondigitalocean.com',
     database: 'film',
     password: 'AVNS_0rXk4wkKmqQa6uONWpo',
     port: 25060,
     ssl: {
         rejectUnauthorized: false
+    }*/
+	
+	//régiek:
+	user: 'doadmin',
+    host: 'db-postgresql-fra1-96846-do-user-12971066-0.b.db.ondigitalocean.com',
+    database: 'film',
+    password: 'AVNS_0rXk4wkKmqQa6uONWpo',
+    port: 25060,
+	ssl: {
+        rejectUnauthorized: false
     }
+	
 });
 
 module.exports = pool;
