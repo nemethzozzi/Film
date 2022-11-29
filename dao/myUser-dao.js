5d0ce9daef9c2ef16616418c24e240d907037866
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-29 20:02:02 +0100

    megnezendo oldal

--- dao/myUser-dao.js
+++ dao/myUser-dao.js
@@ -37,7 +37,7 @@ class myUserDAO {
         //lista vagy array ?
         let megnezendo = [];
         for(let i = 1; i<filmIdk.length-1;i++){
-            film = await db.query("SELECT cim, kepUrl FROM film WHERE filmId = $1", [filmIdk[i]]);
+            film = await db.query("SELECT cim, \"kepUrl\" FROM film WHERE \"filmId\" = $1", [filmIdk[i]]);
             megnezendo.append(film[0]);
             
         }
