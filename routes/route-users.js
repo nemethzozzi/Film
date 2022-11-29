3665a806f469fecf6f5062d5ff17b758bfdc138c
Szűcs Péter Attila <h164804@stud.u-szeged.hu>
2022-11-29 15:55:24 +0100

    ertekeles

--- routes/route-users.js
+++ routes/route-users.js
@@ -164,8 +164,10 @@ router.get("/film", async (req, res) => {
     } let felh_adatok = await new UserDAO().getUserByEmail(email);
 	
 	if(felh_adatok.ertekeltFilmek=="")
-	{
-		await new FilmDAO().leker("update felhasznalo set \"ertekeltFilmek\"=';'");
+		console.log("ertekelt filmek: '"+felh_adatok.ertekeltFilmek+"'");
+		console.log("ertekelt filmek: '"+(typeof felh_adatok.ertekeltFilmek)+"'");
+		await new FilmDAO().leker("update felhasznalo set \"ertekeltFilmek\"=';' where \"felhasznaloId\"="+felh_adatok.felhasznaloId);
+		felh_adatok = await new UserDAO().getUserByEmail(email);
 	}
 	
 	let film = await new FilmDAO().leker("select * from film where \"filmId\" = $1",[req.query.id]);
